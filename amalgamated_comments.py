a4a14a8f6e50f6d5b6e0afcfa22a8648af524edb
BHarris02 <118061899+BHarris02@users.noreply.github.com>
2025-05-05 21:01:57 +0100

    Added amalgamated_comments.py

---
+++ amalgamated_comments.py
@@ -0,0 +1,100 @@
+# Project: jfreechart-1.0.19
+# Classification: WITHOUT_CLASSIFICATION
+# check that the clone doesn't share the same underlying arrays.
+
+# Project: apache-ant-1.7.0
+# Classification: DESIGN
+# This array really shouldn't be here. Again, for historical purposes it is.
+# FIXME: This array should be in a private or package private location, since it could be modified by malicious code.
+
+# Project: jruby-1.4.0
+# Classification: WITHOUT_CLASSIFICATION
+# We do not want to capture cached methods
+
+# Project: argouml
+# Classification: WITHOUT_CLASSIFICATION
+# If the line angle is less than 3 degrees then snap the line // straight
+
+# Project: jEdit-4.2
+# Classification: WITHOUT_CLASSIFICATION
+# Provide the full key path, not just the key name
+
+# Project: sql12
+# Classification: WITHOUT_CLASSIFICATION
+# If you use anonymous you will be logged in and // the pw will be ignored
+
+# Project: apache-ant-1.7.0
+# Classification: DEFECT
+# FIXME: Is "No Namespace is Empty Namespace" really OK?
+
+# Project: jfreechart-1.0.19
+# Classification: WITHOUT_CLASSIFICATION
+# 1-Jan-2000
+
+# Project: jEdit-4.2
+# Classification: WITHOUT_CLASSIFICATION
+# }}}
+
+# Project: apache-jmeter-2.10
+# Classification: WITHOUT_CLASSIFICATION
+# Must be the default resource file
+
+# Project: columba-1.4-src
+# Classification: WITHOUT_CLASSIFICATION
+# Turn off authentication for the future
+
+# Project: columba-1.4-src
+# Classification: WITHOUT_CLASSIFICATION
+# One or many arguments?
+
+# Project: columba-1.4-src
+# Classification: WITHOUT_CLASSIFICATION
+# Add a default PLAIN login as fallback
+
+# Project: jfreechart-1.0.19
+# Classification: WITHOUT_CLASSIFICATION
+# Why are we using floats here?
+
+# Project: columba-1.4-src
+# Classification: WITHOUT_CLASSIFICATION
+# Fallback to Securest Login method
+
+# Project: apache-ant-1.7.0
+# Classification: TEST
+# this is the wrong test if the remote OS is OpenVMS, // but there doesn't seem to be a way to detect it.
+
+# Project: jEdit-4.2
+# Classification: WITHOUT_CLASSIFICATION
+# Avoid creating a new listener for every buffer change
+
+# Project: columba-1.4-src
+# Classification: WITHOUT_CLASSIFICATION
+# Authentication is not supported
+
+# Project: jfreechart-1.0.19
+# Classification: WITHOUT_CLASSIFICATION
+# draw the polygon series...
+
+# Project: jEdit-4.2
+# Classification: WITHOUT_CLASSIFICATION
+# Do not HTML-unescape the attribute value again
+
+# Project: argouml
+# Classification: WITHOUT_CLASSIFICATION
+# The default notation should be loaded first
+
+# Project: columba-1.4-src
+# Classification: WITHOUT_CLASSIFICATION
+# turn off SSL for the future
+
+# Project: jfreechart-1.0.19
+# Classification: WITHOUT_CLASSIFICATION
+# Set the legend background paint to transparent if needed
+
+# Project: apache-ant-1.7.0
+# Classification: DESIGN
+# bingo append it. (this should normally not be done here)
+
+# Project: jEdit-4.2
+# Classification: WITHOUT_CLASSIFICATION
+# If the regex fails, just use the URL as-is
